55417faf60757a0db840914dee4c2aabe17af0f3
ace-yasu <sgt.ace753@gmail.com>
2019-08-17 16:36:36 -0700

    New Commit
    2019.Aug.17

---
+++ Assets/Scripts/tap_button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tap_button : MonoBehaviour
+{
+    public static Card tappedCard;
+
+    public void OnClick()
+    {
+        tappedCard = GetComponent<Card>();
+        Debug.Log("カードを取得しました");
+    }
+}
